ce42ce6376e03df9584f3053e08b73b4554b1f07
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-04-24 21:22:33 +0100

    Email verification page

--- src/pages/verify-email.tsx
+++ src/pages/verify-email.tsx
@@ -1,16 +1,23 @@
+import { signIn } from 'next-auth/react';
+
 export default function VerifyEmail() {
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col justify-around items-center">
-      <h1 className="text-xl">
-        Verify your email
-      </h1>
+      <div>
+        <h1 className="text-xl">
+          Verify your email
+        </h1>
 
-      <p className="text-md">
-        A verification email has been sent to you, please verify your email address.
-        Once you have done so, please log in using the button below.
-      </p>
+        <p className="text-md">
+          A verification email has been sent to you, please verify your email address.
+          Once you have done so, please log in using the button below.
+        </p>
+      </div>
 
-      <button className="bg-red-400 rounded-full p-2 m-4">
+      <button
+        className="bg-red-400 rounded-full p-2 m-4"
+        onClick={() => signIn('firebase', { redirect: true, callbackUrl: '/homepage' })}
+      >
         Login
       </button>
     </div>
