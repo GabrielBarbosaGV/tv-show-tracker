dcd066d0e2db73d3148770e4ed99d270745b86f8
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-04-24 21:21:38 +0100

    Search or create options in homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -6,14 +6,60 @@ import { useMemo } from "react";
 
 export const getServerSideProps = checkUnauthorizedOrEmailNotVerifiedThen(getUserProps);
 
+interface ShadowedBoxProps {
+  children?: React.ReactNode;
+}
+
+const ShadowedBox: React.FC<ShadowedBoxProps> = ({ children }) => {
+  return (
+    <div
+      className="
+        shadow-black
+        shadow-md
+        p-8
+        rounded-md
+        w-[20vw]
+        h-[20vh]
+        text-2xl
+        m-8
+        bg-red-400
+        flex
+        justify-center
+        items-center
+        text-center
+        cursor-pointer
+        active:scale-90
+      "
+    >
+      { children }
+    </div>
+  );
+};
+
 export default function HomePage({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const emailName = useMemo(() => user.email ? getEmailName(user.email) : null, [user.email]);
 
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
-      <h1 className="text-3xl">
-        Welcome Home, {emailName ?? 'you'}!
-      </h1>
+      <div className="">
+        <h1 className="text-3xl m-4">
+          Welcome Home, {emailName ?? 'you'}!
+        </h1>
+
+        <h2 className="text-lg text-center text-black text-opacity-40">
+          What do you feel like doing?
+        </h2>
+      </div>
+
+      <div className="flex flex-row">
+        <ShadowedBox>
+          Add a new show
+        </ShadowedBox>
+
+        <ShadowedBox>
+          Visualize or edit an existing show
+        </ShadowedBox>
+      </div>
     </div>
   );
 }
