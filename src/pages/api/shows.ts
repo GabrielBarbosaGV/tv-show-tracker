cfe0abf52acc5a9af946a0d0fc530728051e7a49
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2023-04-25 22:30:48 +0100

    Responding with JSON in show creation

--- src/pages/api/shows.ts
+++ src/pages/api/shows.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       await addShow(JSON.parse(req.body));
-      res.status(200);
+      res.status(200).json({ success: true });
     } catch (err) {
       console.trace(err);
       res.status(500).json({ message: err });
